bd8ba37a117ba3a37b7dd21f96494053c62eaf8b
LeeSohyeon <teabag759@gmail.com>
2023-11-21 13:37:40 +0900

    Initial Commit

---
+++ I_am_Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I_am_Rich
+//
+//  Created by 이소현 on 11/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
